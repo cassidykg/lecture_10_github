b6348aa2f67056b806795a2583c03c7a800f80c8
cassidykg <150754715+cassidykg@users.noreply.github.com>
2024-11-17 23:43:34 -0500

    leds blink in order

--- lecture_10_github.ino
+++ lecture_10_github.ino
@@ -10,11 +10,12 @@ void setup() {
 
 void loop() {
   digitalWrite(ledPin, HIGH);
-  digitalWrite(ledPin2, HIGH);
-  digitalWrite(ledPin3, HIGH);
   delay(500);
   digitalWrite(ledPin, LOW);
-  digitalWrite(ledPin2, LOW);
-  digitalWrite(ledPin3, LOW);
+  digitalWrite(ledPin2, HIGH);
   delay(500);
+  digitalWrite(ledPin2, LOW);
+  digitalWrite(ledPin3, HIGH);
+  delay(500);
+  digitalWrite(ledPin3, LOW);
 }
